c9db1ad0ef222797e72ca5935d03deebc78deb39
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-15 15:24:39 +0700

    changed some files

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,5 +3,17 @@ fn main() {
 
     s.push_str(", world!"); // push_str() appends a literal to a string
 
+    {
+        let s = String::from("hello");
+        println!(" the value of s is {s}");
+    }
     println!("{s}");
+
+    let x = 7;
+    let y = x;
+
+
+
+
+
 }
