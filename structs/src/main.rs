e99b4a0564689f9545749d7e249851362b93d339
axeladelwyn <85653279+axeladelwyn@users.noreply.github.com>
2024-08-16 21:21:19 +0700

    structs finished

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,23 +1,52 @@
 fn main() {
     println!("Hello, world!");
 
-    let mut  user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 8,
-    };
+    let user1 = build_user(
+        String::from("boris322@gmail.com"),
+        String::from("boris vladislav"),
+        true,
+        3,
+    );
 
-    user1.email = String::from("anotherermail@example.com");
+    println!(
+        "User: {}, Email: {}, Active: {}, Sign-in count: {}",
+        user1.username, user1.email, user1.active, user1.sign_in_count
+    );
 
-    let rect  = Rectangle {
+    let rect = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("The area of the rectangle is {} sqquare pixels.", rect.area());
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
 
+    let user2 = User {
+        email: String::from("asepcobra69@gmail.com"),
+        ..user1.clone()
+    };
 
+    println!(
+        "User: {}, Email: {}, Active: {}, Sign-in count: {}",
+        user2.username, user2.email, user2.active, user2.sign_in_count
+    );
+
+    println!(
+        "User: {}, Email: {}, Active: {}, Sign-in count: {}",
+        user1.username, user1.email, user1.active, user1.sign_in_count
+    );
+
+    let black = Color(12, 12, 14);
+
+    let origin = Point(-24, -34, 25);
+
+    let subject = AlwaysEqual;
+
+    black.display();
+
+    origin.display();
 }
 
 struct Rectangle {
@@ -31,8 +60,7 @@ impl Rectangle {
     }
 }
 
-
-
+#[derive(Clone)]
 struct User {
     active: bool,
     username: String,
@@ -40,11 +68,29 @@ struct User {
     sign_in_count: u64,
 }
 
-fn build_user(email: String, username: String) -> User {
+fn build_user(email: String, username: String, active: bool, sign_in_count: u64) -> User {
     User {
-        active: true,
+        active,
         username,
         email,
-        sign_in_count: 1,
+        sign_in_count,
     }
 }
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+impl Color {
+    // Method to display the color
+    fn display(&self) {
+        println!("Color ({}, {}, {})", self.0, self.1, self.2);
+    }
+}
+
+impl Point {
+    // Method to display the point
+    fn display(&self) {
+        println!("Point: ({}, {}, {})", self.0, self.1, self.2);
+    }
+}
+struct AlwaysEqual;
